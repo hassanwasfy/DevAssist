1fd963ac66f08d8b1689ca1001e33a9387b5df45
abaferas <wasfyelgamed@gmail.com>
2024-01-23 00:55:07 +0200

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,127 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+    id("androidx.navigation.safeargs.kotlin")
+    id("com.google.firebase.crashlytics")
+}
+
+android {
+    namespace = "com.abaferas.devassist"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.abaferas.devassist"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
+    // Dependency Injection
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
+    implementation("androidx.hilt:hilt-work:1.1.0")
+    kapt("androidx.hilt:hilt-compiler:1.1.0")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+    // Lifecycle
+    val lifecycleVersion = "2.6.2"
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation ("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
+
+    implementation ("com.google.accompanist:accompanist-permissions:0.32.0")
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
+    // FirebaseUI for Firebase Realtime Database
+    implementation ("com.firebaseui:firebase-ui-database:8.0.2")
+    // FirebaseUI for Cloud Firestore
+    implementation ("com.firebaseui:firebase-ui-firestore:8.0.2")
+    // FirebaseUI for Firebase Auth
+    implementation ("com.firebaseui:firebase-ui-auth:8.0.2")
+    implementation ("com.google.firebase:firebase-auth:22.3.0")
+    // FirebaseUI for Cloud Storage
+    implementation ("com.firebaseui:firebase-ui-storage:8.0.2")
+
+    implementation("com.google.firebase:firebase-crashlytics")
+    implementation("com.google.firebase:firebase-analytics")
+
+    implementation ("com.google.firebase:firebase-crashlytics:18.6.0")
+    implementation ("com.google.firebase:firebase-analytics:21.5.0")
+    implementation ("com.airbnb.android:lottie-compose:6.2.0")
+
+
+    val roomVersion = "2.6.1"
+
+    implementation ("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor ("androidx.room:room-compiler:$roomVersion")
+    kapt ("androidx.room:room-compiler:$roomVersion")
+    testImplementation ("androidx.room:room-testing:$roomVersion")
+    implementation ("androidx.room:room-paging:$roomVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.0")
+
+}
+
+kapt {
+    correctErrorTypes = true
+}
---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+
+    val navVersion by extra ("2.6.0")
+
+    repositories{
+        google()
+        mavenCentral()
+        maven{
+            url  = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+        }
+        maven{
+            url = uri("https://oss.jfrog.org/artifactory/oss-snapshot-local")
+        }
+    }
+    dependencies{
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.48")
+        classpath ("com.google.gms:google-services:4.4.0")
+        classpath ("com.google.firebase:perf-plugin:1.4.2")
+        classpath ("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
+        classpath ("com.android.tools.build:gradle:8.2.1")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.8.10" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
+    id("com.google.firebase.crashlytics") version "2.9.9" apply false
+    kotlin("kapt") version "1.9.0"
+}
